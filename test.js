e9fb400dc15ebb0e65f1cf74bf363d56785c8833
Michael Wuergler <wuergler@gmail.com>
2016-01-10 00:33:41 +0000

    first draft of API

--- test.js
+++ test.js
@@ -1,6 +1,34 @@
 import test from 'ava';
-import fn from './';
+import LetterSet from './index';
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+
+test('should find letters', t => {
+	const letterset = new LetterSet();
+	t.is(letterset.isLetter('a'), true);
+	t.is(letterset.isLetter('Б'), true);
+	t.is(letterset.isLetter('ş'), true);
+	t.is(letterset.isLetter('Ь'), true);
+	t.is(letterset.isLetter('ƒ'), true);
+});
+
+test('should return all characters', t => {
+	const letterset = new LetterSet();
+	const arr = letterset.allCharacters();
+	t.is(Array.isArray(arr), true);
+	t.true(arr.length > 0);
+});
+
+test('should get hex', t => {
+	const letterset = new LetterSet();
+	t.is(letterset.hex('A'), 0x41);
+});
+
+test('should get decimal', t => {
+	const letterset = new LetterSet();
+	t.is(letterset.decimal('A'), 65);
+});
+
+test('should get name', t => {
+	const letterset = new LetterSet();
+	t.is(letterset.name('Œ'), 'Latin capital ligature O');
 });
